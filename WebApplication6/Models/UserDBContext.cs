d4e1722241a9620b670a93e47c374079f2ebacfe
Meet Soni <98331817+Meetsoni3591@users.noreply.github.com>
2023-09-23 15:57:14 +0530

    Web6
    add the web 6 project

---
+++ WebApplication6/Models/UserDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using WebApplication6.Migrations;
+
+namespace WebApplication6.Models
+{
+    public class UserDBContext : DbContext
+    {
+        public UserDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<UserT> Users { get; set; }     //this is name of Database through dbset 
+    }
+}
